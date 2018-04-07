acd5bbcd05d1246defce2d32022172fd90052bcc
Chris Dundon <chris.dundon@gmail.com>
2018-04-07 02:08:04 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,15 @@
 # Your code goes here!
 class Anagram
-  attr :word
+  attr_accessor :word
   
   def initialize(word)
-    
+    @word = word
   end
   
   def match(arr_words)
+    obj_word = word
+    
+    obj_word = obj_word.split
     
   end
 end
